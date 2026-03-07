2515f6d6269e3734c0b05571ec4bbb4ceadcbf3e
Maverick <romil.agrawal@nyu.edu>
2026-03-07 00:21:38 -0800

    Part 3: Difference Between First and Last Score

---
+++ IMSession11_python_essentials-2/part3.py
@@ -0,0 +1,15 @@
+class StudentPerformance:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def score_difference(self):
+        try:
+            return self.scores[-1] - self.scores[0]
+        except IndexError:
+            print("No scores available to calculate difference")
+            return None
+        
+
+s1 = StudentPerformance([])
+diff1 = s1.score_difference()
+diff1 is not None and print("Difference between last and first score is: ", diff1)
